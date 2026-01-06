7d306a212f00c74e22f03eb17272a51f1516482f
Yuri Almeida <136770806+Yuri-Diego@users.noreply.github.com>
2026-01-06 14:46:22 -0300

    Update comment in main.tf for testing purposes

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# comentario so pra testar filé. 
+# testando filé. 
 
 locals {
   network = "${element(split("-", var.subnet), 0)}"
